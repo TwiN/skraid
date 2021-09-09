04f87a75fdc36397ca3a58b3c3cc8222b0baecb4
TwinProduction <twin@twinnation.org>
2021-09-08 20:13:51 -0400

    Enable anti spam feature

--- src/listeners/events/message.rs
+++ src/listeners/events/message.rs
@@ -13,72 +13,38 @@ pub async fn message(ctx: Context, msg: Message) {
     }
     let is_spamming: bool;
     let mut messages_to_delete: Option<Vec<ChannelAndMessageId>> = None;
-    let mut alert_only: bool = false;
+    let mut alert_only: bool = true;
     let mut alert_channel_id: u64 = 0;
     {
         let data = ctx.data.read().await;
-        let anti_spam_mutex = data.get::<AntiSpam>().unwrap();
-        let mut anti_spam = anti_spam_mutex.lock().unwrap();
-        let db_mutex = data.get::<Database>().unwrap();
-        let db = db_mutex.lock().unwrap();
-        match db.get_guild_configuration(msg.guild_id.unwrap().0) {
-            Ok((a, b, _, _)) => {
-                alert_only = a;
-                alert_channel_id = b;
-                ()
-            }
-            Err(e) => {
-                eprintln!("[{}] Failed to retrieve guild configuration: {}", msg.guild_id.unwrap().0, e.to_string());
-                ()
+        if let Some(db_mutex) = data.get::<Database>() {
+            let db = db_mutex.lock().unwrap();
+            match db.get_guild_configuration(msg.guild_id.unwrap().0) {
+                Ok((a, b, _, _)) => {
+                    alert_only = a;
+                    alert_channel_id = b;
+                    ()
+                }
+                Err(e) => {
+                    eprintln!("[{}] Failed to retrieve guild configuration: {}", msg.guild_id.unwrap().0, e.to_string());
+                    ()
+                }
             }
         }
-        is_spamming = anti_spam.check_if_spamming(msg.guild_id.unwrap().0, msg.author.id.0, msg.channel_id.0, msg.id.0);
-        if is_spamming {
-            log(&ctx, &msg, format!("User {} may be spamming (NO ACTION TAKEN): {}", msg.author.tag(), msg.content));
-            if let Some(channel_and_message_ids) = anti_spam.get_recent_message_ids(msg.guild_id.unwrap().0, msg.author.id.0) {
-                messages_to_delete = Some(channel_and_message_ids.to_vec());
+        if let Some(anti_spam_mutex) = data.get::<AntiSpam>() {
+            let mut anti_spam = anti_spam_mutex.lock().unwrap();
+            is_spamming = anti_spam.check_if_spamming(msg.guild_id.unwrap().0, msg.author.id.0, msg.channel_id.0, msg.id.0);
+            if is_spamming {
+                log(&ctx, &msg, format!("User {} may be spamming (NO ACTION TAKEN): {}", msg.author.tag(), msg.content));
+                if let Some(channel_and_message_ids) = anti_spam.get_recent_message_ids(msg.guild_id.unwrap().0, msg.author.id.0) {
+                    messages_to_delete = Some(channel_and_message_ids.to_vec());
+                }
+                anti_spam.delete_recent_message_ids(msg.guild_id.unwrap().0, msg.author.id.0);
             }
-            anti_spam.delete_recent_message_ids(msg.guild_id.unwrap().0, msg.author.id.0);
         }
     }
     if is_spamming {
-        if alert_only {
-            if alert_channel_id != 0 {
-                let _ = ChannelId(alert_channel_id)
-                    .send_message(&ctx, |m| {
-                        m.add_embed(|e| {
-                            e.description(format!("<@{0}> is currently spamming, but their messages will not be deleted due to `alert_only` being set to `true`", msg.author.id.0,))
-                        })
-                    })
-                    .await;
-            }
-        } else {
-            if let Some(messages) = messages_to_delete {
-                let mut messages_by_channel: HashMap<u64, Vec<MessageId>> = HashMap::new();
-                // split messages by channel so we can bulk delete them
-                for message_to_delete in messages {
-                    if messages_by_channel.contains_key(&message_to_delete.channel_id) {
-                        if let Some(messages_in_channel) = messages_by_channel.get_mut(&message_to_delete.channel_id) {
-                            messages_in_channel.push(MessageId(message_to_delete.message_id));
-                        }
-                    } else {
-                        messages_by_channel.insert(message_to_delete.channel_id, vec![MessageId(message_to_delete.message_id)]);
-                    }
-                }
-                for (channel_id, message_ids) in messages_by_channel.into_iter() {
-                    println!("{} {}", channel_id, message_ids.to_vec().into_iter().map(|i| i.to_string()).collect::<String>());
-                    match ChannelId(channel_id).delete_messages(&ctx, message_ids).await {
-                        Ok(_) => (),
-                        Err(e) => eprintln!("{}", e.to_string()),
-                    }
-                }
-            }
-            if alert_channel_id != 0 {
-                let _ = ChannelId(alert_channel_id)
-                    .send_message(&ctx, |m| m.add_embed(|e| e.description(format!("<@{0}> was spamming, so their messages were automatically deleted", msg.author.id.0,))))
-                    .await;
-            }
-        }
+        handle_spammer(&ctx, &msg, &mut messages_to_delete, alert_only, alert_channel_id).await;
         return;
     }
     // Ignore short messages, unlikely to be noteworthy
@@ -86,18 +52,19 @@ pub async fn message(ctx: Context, msg: Message) {
         return;
     }
     let message_content = msg.content.to_lowercase();
-    let contains_forbidden_word: bool;
+    let mut contains_forbidden_word: bool = false;
     {
         let data = ctx.data.read().await;
-        let mutex = data.get::<Database>().unwrap();
-        let db = mutex.lock().unwrap();
-        contains_forbidden_word = match db.contains_forbidden_word(message_content.to_string()) {
-            Ok(b) => b,
-            Err(e) => {
-                log(&ctx, &msg, format!("Failed to check if message contained forbidden word: {}", e.to_string()));
-                false
-            }
-        };
+        if let Some(mutex) = data.get::<Database>() {
+            let db = mutex.lock().unwrap();
+            contains_forbidden_word = match db.contains_forbidden_word(message_content.to_string()) {
+                Ok(b) => b,
+                Err(e) => {
+                    log(&ctx, &msg, format!("Failed to check if message contained forbidden word: {}", e.to_string()));
+                    false
+                }
+            };
+        }
     }
     if contains_forbidden_word {
         let bot_user = ctx.cache.current_user().await;
@@ -105,26 +72,15 @@ pub async fn message(ctx: Context, msg: Message) {
         let action: String;
         if !alert_only {
             if bot_member.unwrap().permissions(&ctx).await.unwrap().contains(Permissions::MANAGE_MESSAGES) {
-                log(&ctx, &msg, format!("user={} ({}) posted a message containing a forbidden word; action=DELETE: {}", msg.author.tag(), msg.author.id.0, message_content));
                 action = " and the message has been deleted".to_string();
                 let _ = msg.delete(&ctx).await;
             } else {
-                log(
-                    &ctx,
-                    &msg,
-                    format!(
-                        "user={} ({}) posted a message containing a forbidden word; action=ALERT (missing MANAGE_MESSAGES permission): {}",
-                        msg.author.tag(),
-                        msg.author.id.0,
-                        msg.content
-                    ),
-                );
                 action = ", but it was not deleted due to missing MANAGE_MESSAGES permission".into();
             }
         } else {
-            log(&ctx, &msg, format!("user={} ({}) posted a message containing a forbidden word; action=ALERT: {}", msg.author.tag(), msg.author.id.0, message_content));
             action = ", but no action was taken due to being in alert-only mode".into();
         }
+        log(&ctx, &msg, format!("User {} ({}) posted a message containing a forbidden word{}: {}", msg.author.tag(), msg.author.id.0, action, message_content));
         if alert_channel_id != 0 {
             let _ = ChannelId(alert_channel_id)
                 .send_message(&ctx, |m| {
@@ -148,5 +104,45 @@ pub async fn message(ctx: Context, msg: Message) {
         && (message_content.contains("free") || message_content.contains("nitro") || message_content.contains("skin") || message_content.contains("win"))
     {
         log(&ctx, &msg, format!("Potentially suspicious message: {}", msg.content.to_string()));
+    }
+}
+
+async fn handle_spammer(ctx: &Context, msg: &Message, messages_to_delete: &mut Option<Vec<ChannelAndMessageId>>, alert_only: bool, alert_channel_id: u64) {
+    if alert_only {
+        if alert_channel_id != 0 {
+            let _ = ChannelId(alert_channel_id)
+                .send_message(&ctx, |m| {
+                    m.add_embed(|e| {
+                        e.description(format!("<@{0}> is currently spamming, but their messages will not be deleted due to `alert_only` being set to `true`", msg.author.id.0,))
+                    })
+                })
+                .await;
+        }
+    } else {
+        if let Some(messages) = messages_to_delete {
+            let mut messages_by_channel: HashMap<u64, Vec<MessageId>> = HashMap::new();
+            // split messages by channel so we can bulk delete them
+            for message_to_delete in messages {
+                if messages_by_channel.contains_key(&message_to_delete.channel_id) {
+                    if let Some(messages_in_channel) = messages_by_channel.get_mut(&message_to_delete.channel_id) {
+                        messages_in_channel.push(MessageId(message_to_delete.message_id));
+                    }
+                } else {
+                    messages_by_channel.insert(message_to_delete.channel_id, vec![MessageId(message_to_delete.message_id)]);
+                }
+            }
+            for (channel_id, message_ids) in messages_by_channel.into_iter() {
+                //println!("{} {}", channel_id, message_ids.to_vec().into_iter().map(|i| i.to_string()).collect::<String>());
+                match ChannelId(channel_id).delete_messages(&ctx, message_ids).await {
+                    Ok(_) => (),
+                    Err(e) => eprintln!("{}", e.to_string()),
+                }
+            }
+        }
+        if alert_channel_id != 0 {
+            let _ = ChannelId(alert_channel_id)
+                .send_message(&ctx, |m| m.add_embed(|e| e.description(format!("<@{0}> was spamming, so their messages were automatically deleted", msg.author.id.0,))))
+                .await;
+        }
     }
 }
