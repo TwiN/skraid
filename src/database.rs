6e56a7e457f21ffbdfbe579b94b4ccb76ee01767
TwinProduction <twin@twinnation.org>
2021-09-08 22:38:00 -0400

    Remove migration queries

--- src/database.rs
+++ src/database.rs
@@ -38,7 +38,6 @@ impl Database {
             Ok(_) => (),
             Err(error) => panic!("{}", error),
         }
-        let _ = self.connection.execute("ALTER TABLE blocklist RENAME TO user_blocklist;", []);
         match self.connection.execute(
             "CREATE TABLE IF NOT EXISTS guilds (
                 guild_id                 UNSIGNED BIG INT PRIMARY KEY,
@@ -53,10 +52,6 @@ impl Database {
             Ok(_) => (),
             Err(error) => panic!("{}", error),
         }
-        // TODO: remove this once the migration is completed.
-        let _ = self.connection.execute("ALTER TABLE guilds ADD ban_new_user_on_join INTEGER DEFAULT FALSE", []);
-        // TODO: remove this once the migration is completed.
-        let _ = self.connection.execute("ALTER TABLE guilds ADD ban_user_on_join INTEGER DEFAULT FALSE", []);
         match self.connection.execute(
             "CREATE TABLE IF NOT EXISTS user_allowlist (
                 id         INTEGER PRIMARY KEY,
@@ -69,7 +64,6 @@ impl Database {
             Ok(_) => (),
             Err(error) => panic!("{}", error),
         }
-        let _ = self.connection.execute("ALTER TABLE allowlist RENAME TO user_allowlist;", []);
         match self.connection.execute(
             "CREATE TABLE IF NOT EXISTS word_blocklist (
                 id    INTEGER PRIMARY KEY,
@@ -80,7 +74,6 @@ impl Database {
             Ok(_) => (),
             Err(error) => panic!("{}", error),
         }
-        let _ = self.connection.execute("ALTER TABLE forbidden_words RENAME TO word_blocklist;", []);
         println!("Successfully initiated schema");
         let _ = self.insert_in_word_blocklist("discordgift.ru.com".to_string());
         let _ = self.insert_in_word_blocklist("discord-nitro.link".to_string());
